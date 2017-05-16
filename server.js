15af1d8909b74857967f698b8a36bfda12808d63
Mathieu-R <mathieu0709@gmail.com>
2017-05-16 21:19:31 +0200

    fix build

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const http = require('http');
 const path = require('path');
+const fs = require('mz/fs');
 const express = require('express');
 const app = express();
-const router = express.Router();
 const adaro = require('adaro');
 
 const production = process.env.NODE_ENV === 'production';
@@ -10,13 +10,21 @@ const port = process.env.PORT || 3000;
 const staticPath = './static';
 const templatePath = './templates';
 
-router.use(express.static(staticPath, {
+const options = {
+  cache: production ? true : false,
+  whitespace: production ? false : true,
+  helpers: []
+};
+
+app.use(express.static(staticPath, {
   maxAge: production ? (365 * 24 * 60 * 60 * 1000) : 0
 }));
 
-router.get('/check', res => res.send('server ok.'));
+app.engine('dust', adaro.dust(options));
+app.set('view engine', 'dust');
+app.set('views', templatePath);
 
-app.use('router');
+app.get('/check', res => res.send('server ok.'));
 
 http.createServer(app).listen(port, () => {
   console.log(`Portfolio running on http://localhost:${port}`);
