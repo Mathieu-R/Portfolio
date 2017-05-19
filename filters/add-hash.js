bdbde1c3f2329b448e3a249900954032c7538137
Mathieu-R <mathieu0709@gmail.com>
2017-05-19 17:55:32 +0200

    hash working

--- filters/add-hash.js
+++ filters/add-hash.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const crypto = require('crypto');
 
-function readfile(path) {
+function readFileContent(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, (error, content) => {
       if (error) {
@@ -19,11 +19,16 @@ function createHash(content) {
       .digest('hex');
 }
 
-async function hash(dust) {
-  dust.filters.hash = path => {
-    const content = await readfile(path);
-    const hash = createHash(content);
-    return path.replace(/([^\.]+)\.(.+)/, `$1.${hash}.$2`);
+function hash(dust) {
+  dust.helpers.hash = (chunk, context, bodies, params) => {
+    return chunk.map(async chunk => {
+      const path = params.path;
+      const content = await readFileContent(path);
+      const hash = await createHash(content);
+      const hashedPath = path.replace(/([^\.]+)\.(.+)/, `$1.${hash}.$2`);
+      console.log(hashedPath);
+      return chunk.write(hashedPath).end();
+    });
   }
 }
 
