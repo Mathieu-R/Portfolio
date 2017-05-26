07d1e0f8b1364f2e96102f93a9464ee0f1d34b50
Mathieu-R <mathieu0709@gmail.com>
2017-05-26 22:55:30 +0200

    track sw state

--- scripts/sw-install.js
+++ scripts/sw-install.js
@@ -1,9 +1,21 @@
+import Toast from './toast.js';
+
 export function serviceWorkerInstall() {
   if (!('serviceWorker' in navigator)) {
     return;
   }
 
   navigator.serviceWorker.register('/sw.js', {'scope': '/'})
-    .then(() => console.log('Service Worker installing...'))
-    .catch(error => console.warn(error));
+    .then(reg => {
+      console.log('Service Worker installing...');
+      reg.onupdatefound = evt => {
+        Toast.Push('Portfolio updated. Refresh to get the new version.');
+
+        reg.installing.onstatechange = _ => {
+          console.log(`Service Worker ${reg.installing.state}`)
+        }
+
+        console.log(evt);
+      }
+    }).catch(error => console.warn(error));
 }
