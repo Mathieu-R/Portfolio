65d74b98fec29e335816d9a5c0f16b4800894eb8
Mathieu-R <mathieu0709@gmail.com>
2017-05-28 21:44:45 +0200

    templates done

--- filters/normalize-date.js
+++ filters/normalize-date.js
@@ -1,6 +1,9 @@
 function normalizeDate(dust) {
-  dust.filters.normalizeDate = value => {
-
+  dust.filters.normalizeDate = date => {
+    const splittedDate = date.split('-');
+    const year = splittedDate[0];
+    const month = splittedDate[1];
+    const day = splittedDate[2];
   }
 }
 
