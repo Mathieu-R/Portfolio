3e254698c60b5b924ee66a5319ef246c5c3764bc
Mathieu-R <mathieu0709@gmail.com>
2017-06-05 18:16:46 +0200

    custom-elements polyfill

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,19 @@
+import {loadScript} from './utils/utils.js';
 import {serviceWorkerInstall} from './sw-install.js';
 import Toast from './toast.js';
 
 class App {
   constructor() {
     serviceWorkerInstall();
+    this.initCustomElements();
+  }
+
+  initCustomElements () {
+    if (!('customElements' in window)) {
+      loadScript('/static/js/third_party/webcomponents-lite.js').then(_ => {
+        console.log('custom-elements polyfill added.');
+      });
+    }
   }
 }
 
