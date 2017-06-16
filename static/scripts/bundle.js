f10daedccbd927c3ce55eff15a0074643a53cee1
Mathieu-R <mathieu0709@gmail.com>
2017-06-16 13:14:03 +0000

    design

--- static/scripts/bundle.js
+++ static/scripts/bundle.js
@@ -19,10 +19,8 @@ class Toast {
     toast.classList.add('toast');
     toastContent.classList.add('toast-content');
     toastContent.textContent = message;
-
     container.appendChild(toast);
     toast.appendChild(toastContent);
-
     setTimeout(() => toast.classList.add('hide'), 3000);
     toast.addEventListener('transitionend', evt => evt.target.parentNode.removeChild(evt.target));
   }
@@ -32,26 +30,21 @@ function serviceWorkerInstall() {
   if (!('serviceWorker' in navigator)) {
     return;
   }
-
   navigator.serviceWorker.onmessage = evt => {
     if (evt.data.type === 'version') {
       console.log(`Service Worker updated to version ${evt.data.version}`);
       Toast.Push('Portfolio updated. Refresh to get the new version.');
       return;
     }
-
     if (evt.data.type === 'cached') {
       Toast.Push('Portfolio cached. Future visit will work offline !');
       return;
     }
   };
-
   navigator.serviceWorker.register('/sw.js', { 'scope': '/' }).then(reg => {
     reg.onupdatefound = evt => {
       console.log('A new service worker has been found, installing...');
-
       reg.installing.onstatechange = evt => {
-        // as the service worker is installed, we can push the message
         if (evt.target.state === 'activated') {
           reg.active.postMessage('version');
         }
@@ -66,7 +59,6 @@ class App {
     serviceWorkerInstall();
     this.initCustomElements();
   }
-
   initCustomElements() {
     if (!('customElements' in window)) {
       loadScript('/static/js/third_party/webcomponents-lite.js').then(_ => {
@@ -75,6 +67,4 @@ class App {
     }
   }
 }
-
 window.addEventListener('load', _ => new App());
-//# sourceMappingURL=bundle.js.map
